bc41a97bb5b301ecb5b7c7eddad5d45985aec9cf
Dameon Brown <axel12gt@live.com>
2019-06-12 16:32:44 -0400

    added folder structure

---
+++ assets/js/code.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCXN6C6aX8zPkVf7eNdrpREwsicT9K4Cek",
+    authDomain: "train-activity-976f2.firebaseapp.com",
+    databaseURL: "https://train-activity-976f2.firebaseio.com",
+    projectId: "train-activity-976f2",
+    storageBucket: "train-activity-976f2.appspot.com",
+    messagingSenderId: "716338364140",
+    appId: "1:716338364140:web:a12b1f8b4c3fffb2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
