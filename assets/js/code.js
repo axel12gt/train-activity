590e2467926805f8a9c3045ad4c4c9759770534e
Dameon Brown <axel12gt@live.com>
2019-06-15 10:43:52 -0400

    a couple finishing touches

--- assets/js/code.js
+++ assets/js/code.js
@@ -10,17 +10,19 @@
   }
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig)
-
+  // sets dataRef variable to firebase.database()
   var dataRef = firebase.database()
 
+  // names and initializes variables required for the application
   var name = ""
   var dest = ""
   var first = ""
   var freq = ""
 
 
-
+  //my objects 
   var elements = {
+    //gets values from form
     createRow: function () {
 
       //Gets the values from the form
@@ -29,54 +31,67 @@
       first = $("#first").val().trim()
       freq = $("#freq").val().trim()
 
-      console.log(name, dest, first, freq)
     },
-
+    //pushes info from input to database
     ref: function () {
-      console.log("isworking")
+      
+        //database magic that pushes an object to an object
       dataRef.ref().push({
+        //train name from form to database obj
         name: name,
+        //destination from form to database obj
         dest: dest,
+        //first train from form to database obj
         first: first,
+        //frequency from form to database obj
         freq: freq,
+        //not really sure why I put this here
         dateAdded: firebase.database.ServerValue.TIMESTAMP
       })},
-
+      //method that calls firebase and displays to screen
     display: function(){
-
+      //empties table body contents before appending to prevent duplication
       $("tbody").empty()
+        //accesses firebase to display to page
         dataRef.ref().on("child_added", function(childSnapshot) {
+          // sets up calculations to work
           var firstTimeConverted = moment(childSnapshot.val().first, "HH:mm").subtract(1, "years");
-          // console.log(moment(firstTimeConverted),"HH:mm")
+          // difference of now and firstTimeConverted
           var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-          console.log(diffTime)
+          // modulus of calculations
           var tRemainder = diffTime % childSnapshot.val().freq;
-          console.log(tRemainder)
+          // minutes till the next train
           var tMinutesTillTrain = childSnapshot.val().freq - tRemainder;
-          console.log(tMinutesTillTrain)
+          // time of next train HH:mm
           var minutesAway = moment().add(tMinutesTillTrain, "minutes");
-          console.log(moment(minutesAway).format("hh:mm"))
           
+          //calls back firebase values and appends to table
           $("tbody").append("<tr><td> " +
           childSnapshot.val().name +
           " </td><td> " + childSnapshot.val().dest +
           " </td><td> " + childSnapshot.val().freq +
           " </td><td> " + childSnapshot.val().first +
           " </td><td> " + moment(minutesAway).format("hh:mm") +
-          " </td><td> " + moment(tMinutesTillTrain) + "</td></tr>");
+          " </td><td> " + moment(tMinutesTillTrain).format("minutes") + "</td></tr>");
         })
       
 
     }
   }
-
+// does a lot of stuff
   $("button").on("click", function(event){
-    console.log("is working")
+    // prevents the page from refreshing
     event.preventDefault()
+    // doesn't create rows but adds information from the form to temp variables
     elements.createRow()
+    // adds the variables to the firebase as a child element
     elements.ref()
+    // calls the firebase database and returns the values
     elements.display()
   })
-
+  // calls the display function to pull info from the firebase
   elements.display()
-  
+  // refreshes the display every 15 seconds
+  setInterval(() => { elements.display()
+    
+  }, 15000);
